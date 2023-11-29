a19299a550177212b1b8ee44dcb14484ebeb8531
dodotdot <rm.herwidodo@gmail.com>
2023-11-29 11:22:43 +0700

    update gtag

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(VueGtag, {
+    appName:'Kebonkross',
+    pageTrackerScreenviewEnabled: true,
+    config: {
+        id: 'G-PNT7XW151X'
+    }
+})
 app.mount('#app')
