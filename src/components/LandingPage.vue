9570999753f40d71d575e046f09f7723dccf9503
dodotdot <rm.herwidodo@gmail.com>
2023-11-29 10:45:06 +0700

    update bghero

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -34,7 +34,7 @@
                     </div>
                 </div>
             </div>
-            <video autoplay muted loop class="absolute z-10 w-auto min-w-full min-h-full max-w-none">
+            <video autoplay muted loop class="absolute z-10 w-auto min-w-full min-h-full max-w-none"  poster="@/assets/img/bg-hero.jpg">
                 <source
                     src="@/assets/videobg.mp4"
                     type="video/mp4" />
