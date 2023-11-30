85d188f97d966a5af33dd97e6f61be8fa39b8bf0
dodotdot <rm.herwidodo@gmail.com>
2023-11-30 10:41:56 +0000

    update margin top cat

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -158,7 +158,7 @@
                             </article>
                             
                         </div>
-                        <div class="grid gap-4 lg:grid-cols-3">
+                        <div class="mt-4 grid gap-4 lg:grid-cols-3">
                             <article
                                 class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                                 <img class="h-auto"
