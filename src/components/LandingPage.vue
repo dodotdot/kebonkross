6203779a8f1975fd2b3b55d173e502dda2017783
dodotdot <rm.herwidodo@gmail.com>
2024-10-21 21:17:15 +0700

    update tiktok video

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,42 +1,39 @@
 <template>
     <section id="hero">
         <div class="relative flex items-center justify-center h-full w-full bg-cover overflow-hidden backdrop-bg">
-            <div class="relative z-30 p-5 text-white bg-black h-full w-full bg-opacity-70 "
-                >
+            <div class="relative z-30 p-5 text-white bg-black h-full w-full bg-opacity-70 ">
                 <div class="w-full bg-cover ">
                     <div class="flex items-center justify-center w-full h-full bg-opacity-50 py-8">
                         <div class="text-center">
                             <div class="container px-4 mx-auto">
                                 <div class="max-w-4xl mx-auto text-center">
-                                    <img class="w-12 lg:w-20 p-2 mx-auto  mb-12"
-                                        src="@/assets/img/cx-id-logo.png"
+                                    <img class="w-12 lg:w-20 p-2 mx-auto  mb-12" src="@/assets/img/cx-id-logo.png"
                                         alt="">
                                     <img class="mx-auto w-3/4" src="@/assets/logo-kebonkross.png" alt="">
 
                                     <h1
                                         class="my-8 text-3xl font-extrabold leading-none text-white md:text-3xl lg:text-3xl dark:text-white">
                                         Sabtu, 30 November 2024</h1>
-                                     <p class="text-lg mb-5 -mt-4">Start 07:00 AM - Until Done</p>   
-                                     <div class=" text-white text-md w-full mx-2 p-2 mb-6">
-                                         <vue3-flip-countdown 
-                                         class="p-2 bg-white mx-3 w-1/2" 
-                                         countdownSize="1.5rem"
-                                        labelSize=".5rem"
-                                        deadline="2024-11-30 00:00:00" />
-                                     </div>   
-                                     
+                                    <p class="text-lg mb-5 -mt-4">Start 07:00 AM - Until Done</p>
+                                    <div class=" text-white text-md w-full mx-2 p-2 mb-6">
+                                        <vue3-flip-countdown class="p-2 bg-white mx-3 w-1/2" countdownSize="1.5rem"
+                                            labelSize=".5rem" deadline="2024-11-30 00:00:00" />
+                                    </div>
+
                                     <p class="max-w-3xl mx-auto mb-10 text-md text-gray-300">
                                         KEBONKROSS adalah "Lebaran-nya" Cyclocross
                                         Indonesia (CXID). <br>
-                                        KEBONKROSS sendiri merupakan acara kumpulnya pesepeda yang dibalut dengan kegiatan balap secara umum
-                                        namun tetap dengan nuansa balap ala cyclocross, dimana lintasan balap berupa tanah yang dipadu dengan beberapa rintangan buatan maupun alami.
+                                        KEBONKROSS sendiri merupakan acara kumpulnya pesepeda yang dibalut dengan
+                                        kegiatan balap secara umum
+                                        namun tetap dengan nuansa balap ala cyclocross, dimana lintasan balap berupa
+                                        tanah yang dipadu dengan beberapa rintangan buatan maupun alami.
                                     </p>
 
                                     <p class="text-xl my-6 font-semibold">KEBONKROSS 2024 - Registration is now open</p>
                                     <a class="inline-block bg-green-600 cursor-pointer w-full md:w-auto mx-auto mb-4 md:mr-6 py-5 px-8 text-sm font-bold uppercase border-2 border-transparent rounded hover:bg-green-800 text-white transition duration-200"
                                         href="#registernow"><span class="text-lg">👋🏼</span> Daftar Sekarang</a>
                                     <p class="mt-10 text-lg my-6 font-semibold mx-auto cursor-pointer underline">
-                                       <a href="https://tally.so/r/nWMyoR" target="_blank">✨ Pendaftaran tenant ✨</a>
+                                        <a href="https://tally.so/r/nWMyoR" target="_blank">✨ Pendaftaran tenant ✨</a>
                                     </p>
 
                                 </div>
@@ -69,8 +66,10 @@
                                 class="mb-4 text-3xl font-extrabold tracking-wide leading-none text-white md:text-2xl lg:text-3xl dark:text-white">
                                 Lintasan 🎢</h1>
                             <p class="max-w-3xl mx-auto mb-10 text-md text-gray-300">
-                                Lintasan balap berupa sirkuit tanah dilengkapi dengan artificial obstacle. Panjang lintasan
-                                kurang lebih 300 meter untuk 1 lap, dengan lebar 2-3 meter. Obstacle terdiri dari plank barrier,
+                                Lintasan balap berupa sirkuit tanah dilengkapi dengan artificial obstacle. Panjang
+                                lintasan
+                                kurang lebih 300 meter untuk 1 lap, dengan lebar 2-3 meter. Obstacle terdiri dari plank
+                                barrier,
                                 mud, dan lain-lain.
                             </p>
                             <p class="mb-4 text-lg">
@@ -80,8 +79,10 @@
                             <div class="mx-auto max-h-full" style="height:500px;">
                                 <!-- <iframe class="w-full h-full"
                                     src="https://www.google.com/maps/embed/v1/place?key=AIzaSyARNP6PBrYdKNmsYf09tU8jRnffQb-sFQg&amp;q=6%C2%B016%2721.4%22S+106%C2%B043%2705.2%22E"></iframe> -->
-                                    <iframe class="w-full h-full"
-                                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1982.8384094280814!2d106.76064353852516!3d-6.306122998420768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMTgnMjIuMCJTIDEwNsKwNDUnNDMuMCJF!5e0!3m2!1sen!2sus!4v1729431983589!5m2!1sen!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                <iframe class="w-full h-full"
+                                    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1982.8384094280814!2d106.76064353852516!3d-6.306122998420768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMTgnMjIuMCJTIDEwNsKwNDUnNDMuMCJF!5e0!3m2!1sen!2sus!4v1729431983589!5m2!1sen!2sus"
+                                    style="border:0;" allowfullscreen="" loading="lazy"
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
 
@@ -99,16 +100,22 @@
             <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
                 <div class="text-center">
                     <div class="container px-4 mx-auto">
+                     
+                        <iframe class="mx-auto w-3/4" height="500" width="300" src="https://www.tiktok.com/player/v1/7428222255148240134?&music_info=1&description=1&autoplay=1&loop=1" allow="fullscreen" title="test"></iframe>
+<br />
                         <h2
-                                class="my-8 italic text-3xl tracking-wide lg:text-4xl font-extrabold text-white dark:text-white">
-                                “KEBONKROSS IS WRESTLING 🤼‍♂️”</h2>
+                            class="my-8 italic text-3xl tracking-wide lg:text-4xl font-extrabold text-white dark:text-white">
+                            “KEBONKROSS IS WRESTLING 🤼‍♂️”</h2>
                         <p class="p-2 text-white">Bersiaplah untuk tantangan penuh adrenalin di Kebonkross 2024!
-Event ini bukan sekadar balapan sepeda biasa, melainkan sebuah mini cyclocross yang kami desain khusus untuk menguji kekuatan, ketangkasan, dan fokus setiap peserta. 
-Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling—di mana setiap peserta harus menghadapi berbagai rintangan, mulai dari gowes, turun, mengangkat sepeda, hingga melewati kolam air yang menantang. Setiap detik waktu membutuhkan konsentrasi penuh dan stamina tenaga yang prima.</p>
-                        <h2
-                                class="my-8 text-3xl tracking-wide lg:text-4xl font-extrabold text-white dark:text-white">
-                                Peraturan Umum</h2>
-                         <div class="w-1/2 m-auto">
+                            Event ini bukan sekadar balapan sepeda biasa, melainkan sebuah mini cyclocross yang kami
+                            desain khusus untuk menguji kekuatan, ketangkasan, dan fokus setiap peserta.
+                            Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling—di mana
+                            setiap peserta harus menghadapi berbagai rintangan, mulai dari gowes, turun, mengangkat
+                            sepeda, hingga melewati kolam air yang menantang. Setiap detik waktu membutuhkan konsentrasi
+                            penuh dan stamina tenaga yang prima.</p>
+                        <h2 class="my-8 text-3xl tracking-wide lg:text-4xl font-extrabold text-white dark:text-white">
+                            Peraturan Umum</h2>
+                        <div class="w-1/2 m-auto">
                             <article
                                 class="p-4 bg-white w-full rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                                 <ol
@@ -127,7 +134,7 @@ Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling�
         </div>
     </section>
 
-    
+
 
     <section id="category">
         <div class="w-full bg-center bg-cover"
@@ -143,12 +150,10 @@ Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling�
                         <div class="grid gap-4 lg:grid-cols-3">
                             <article
                                 class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="h-auto"
-                                    src="@/assets/img/cx.jpg">
+                                <img class="h-auto" src="@/assets/img/cx.jpg">
                                 <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                         href="#">Cyclocross (Men Open)</a></h2>
-                                <ul
-                                    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                                     <li>
                                         700c, max 35mm tire wide, max dropbar flare 15deg
                                     </li>
@@ -156,53 +161,34 @@ Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling�
                             </article>
                             <article
                                 class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="h-auto"
-                                    src="@/assets/img/tracklo.jpg">
+                                <img class="h-auto" src="@/assets/img/tracklo.jpg">
                                 <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                         href="#">Tracklocross (Men Open)</a></h2>
-                                <ol
-                                    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                <ol class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                                     <li>
-                                        Fixed gear, No brakes 
+                                        Fixed gear, No brakes
                                     </li>
                                 </ol>
                             </article>
                             <article
                                 class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="h-auto"
-                                    src="@/assets/img/mtb26.jpg">
+                                <img class="h-auto" src="@/assets/img/mtb26.jpg">
                                 <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                         href="#">Vintage 26" (Men Open)</a></h2>
-                                <ol
-                                    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                <ol class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                                     <li>All MTB production before 2001
                                     </li>
                                 </ol>
                             </article>
-                            
+
                         </div>
                         <div class="mt-4 grid gap-4 lg:grid-cols-3">
                             <article
                                 class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="h-auto"
-                                    src="@/assets/img/ffa-men.jpg">
+                                <img class="h-auto" src="@/assets/img/ffa-men.jpg">
                                 <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                         href="#">FFA/Free For All (Men Open)</a></h2>
-                                <ol
-                                    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
-                                    <li>
-                                        Full rigid, non-MTB, dropbar/flatbar
-                                    </li>
-                                </ol>
-                            </article>
-                             <article
-                                class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="h-auto"
-                                    src="@/assets/img/ffa-women.jpg">
-                                <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
-                                        href="#">FFA/Free For All (Women Open)</a></h2>
-                                <ol
-                                    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                <ol class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                                     <li>
                                         Full rigid, non-MTB, dropbar/flatbar
                                     </li>
@@ -210,12 +196,21 @@ Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling�
                             </article>
                             <article
                                 class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="h-auto"
-                                    src="@/assets/img/heavyweight.jpg">
+                                <img class="h-auto" src="@/assets/img/ffa-women.jpg">
+                                <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
+                                        href="#">FFA/Free For All (Women Open)</a></h2>
+                                <ol class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                    <li>
+                                        Full rigid, non-MTB, dropbar/flatbar
+                                    </li>
+                                </ol>
+                            </article>
+                            <article
+                                class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                                <img class="h-auto" src="@/assets/img/heavyweight.jpg">
                                 <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
                                         href="#">Heavyweight (Men Open)</a></h2>
-                                <ol
-                                    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                <ol class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                                     <li>All kind bikes except e-bike</li>
                                     <li>Minimum weight 90kg size dan/atau lingkar pinggang min 90cm</li>
                                 </ol>
@@ -231,8 +226,7 @@ Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling�
         <iframe
             data-tally-src="https://tally.so/embed/waDGPX?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
             loading="lazy" width="100%" height="2090" frameborder="0" marginheight="0" marginwidth="0"
-            title="KEBON KROSS CX-ID 2024 RACE "
-            ></iframe>
+            title="KEBON KROSS CX-ID 2024 RACE "></iframe>
     </section>
 
     <section id="sponsor" class="bg-white dark:bg-gray-900 mt-12">
@@ -310,7 +304,8 @@ Dalam event ini, Anda akan merasakan pengalaman mirip dengan olahraga wrestling�
         </div> -->
 
 
-</section></template>
+    </section>
+</template>
 <style scoped>
 .backdrop-bg {
     background-image: url('@/assets/img/bg-hero.jpeg'); /* Correct path */
